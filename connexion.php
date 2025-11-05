112f6e7294c9298c0ffe4b8daba680b2e704d24f
Raphaël Mourtzanakis <raphael.mourtzanakis@lmdsio.fr>
2025-11-05 17:43:12 +0100

    Version 1.1

--- connexion.php
+++ connexion.php
@@ -1,5 +1,23 @@
 <?php
-    require_once 'configs.php';
+require_once __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load(); // charge les variables d’environnement avec la méthode load() de la classe Dotenv
+
+// Paramètres de la base de données
+$host = $_ENV['DB_HOST']; // l'hôte de la base de données
+$database = $_ENV['DB_NAME']; // nom de la base de données
+$user_mysql = $_ENV['DB_USER']; // nom d'utilisateur de l'utilisateur de MySQL
+$password_mysql = $_ENV['DB_PASS']; // mot de passe de l'utilisateur de MySQL
+
+// Mode DEBUG
+$debug = filter_var($_ENV['DEBUG'], FILTER_VALIDATE_BOOLEAN);
+
+// Connexion à la base de données
+$db = mysqli_connect($host, $user_mysql, $password_mysql, $database);
+mysqli_set_charset($db, "utf8");
+if ($debug) {
+    echo "Connexion à la base de données réussie";
+}
 ?>
 
 <!DOCTYPE html>
